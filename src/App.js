323939bc231b572f33456fa751ce8340fba8d682
caiobaraujo <caiobrega@gmail.com>
2021-10-31 18:10:20 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
 import './App.css';
+import ChatListItem from './components/ChatListItem';
 
-function App() {
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+  const [chatlist, setChatList] = useState([{}, {}, {}, {}]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-window">
+      <div className="sidebar">
+        <header>
+          <img
+            className="header--avatar"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4XSv8zvv1XvIvvz8PFg8hRwUTQJjbis-CJg&usqp=CAU"
+            alt=""
+          />
+          <div className="header--buttons">
+            <div className="header--btn">
+              <DonutLargeIcon style={{ color: '#919191' }} />
+            </div>
+            <div className="header--btn">
+              <ChatIcon style={{ color: '#919191' }} />
+            </div>
+            <div className="header--btn">
+              <MoreVertIcon style={{ color: '#919191' }} />
+            </div>
+          </div>
+        </header>
+        <div className="search">
+          <div className="search--input">
+            <SearchIcon fontSize="small" style={{ color: '#919191' }} />
+            <input
+              type="search"
+              placeholder="Procurar ou começar uma nova conversa"
+            ></input>
+          </div>
+        </div>
+        <div className="chatlist">
+          {chatlist.map((item, key) => (
+            <ChatListItem key={key} />
+          ))}
+        </div>
+      </div>
+      <div className="contentarea">...</div>
     </div>
   );
-}
-
-export default App;
+};
---
+++ src/components/ChatListItem.js
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from 'react';
+import './ChatListItem.css';
+
+export default () => {
+  return <div>...</div>;
+};
